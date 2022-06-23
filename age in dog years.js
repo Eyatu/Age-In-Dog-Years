f6a08053a220e33f078da0354a90cc52eec68e87
DemirB <demir.beynam@solvoyo.com>
2022-06-23 13:42:21 +0300

    Add simple tool that converts human age to dog equivalent

---
+++ age in dog years.js
@@ -0,0 +1,13 @@
+let myAge= 17
+//Created age variable
+let earlyYears= 2
+//Created changable variable
+earlyYears*= 10.5
+let laterYears= myAge - 2
+//Created new variable
+laterYears*= 4
+let myAgeInDogYears= earlyYears + laterYears
+const myName= 'Demir'
+myName.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+//showing results
